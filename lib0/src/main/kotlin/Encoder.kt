897a08978032b06eb19619b80df62d4d52ebce08
Riguz Lee <dr@riguz.com>
2025-03-18 18:19:01 +0800

    added encoder

--- lib0/src/main/kotlin/Encoder.kt
+++ lib0/src/main/kotlin/Encoder.kt
@@ -1,6 +1,7 @@
 package com.riguz.y.lib0
 
 import java.io.ByteArrayOutputStream
+import java.nio.charset.StandardCharsets
 
 @OptIn(ExperimentalUnsignedTypes::class)
 interface Encoder {
@@ -20,88 +21,151 @@ interface Encoder {
     fun writeVarInt16(number: Short)
     fun writeVarInt32(number: Int)
     fun writeVarInt64(number: Long)
-    fun writeVarUint8Array(number: UByteArray)
+    fun writeUint8Array(array: UByteArray)
+    fun writeVarUint8Array(array: UByteArray)
     fun writeVarString(str: String)
     fun toBytes(): UByteArray
 }
 
 @ExperimentalUnsignedTypes
-class ByteArrayEncoder(var buffer: ByteArrayOutputStream) : Encoder {
+class ByteArrayEncoder(private val buffer: ByteArrayOutputStream) : Encoder {
     override fun writeUint8(number: UByte) {
         buffer.write(number.toInt());
     }
 
     override fun writeUint16(number: UShort) {
-        TODO("Not yet implemented")
+        val intValue = number.toInt()
+        val lsb = (intValue and 0xFF).toByte()
+        val msb = (intValue shr 8 and 0xFF).toByte()
+        buffer.write(byteArrayOf(lsb, msb))
     }
 
     override fun writeUint32(number: UInt) {
-        TODO("Not yet implemented")
+        val intValue = number.toInt()
+        val b1 = (intValue and 0xFF).toByte()
+        val b2 = (intValue shr 8 and 0xFF).toByte()
+        val b3 = (intValue shr 16 and 0xFF).toByte()
+        val b4 = (intValue shr 24 and 0xFF).toByte()
+        buffer.write(byteArrayOf(b1, b2, b3, b4))
     }
 
     override fun writeUint32BigEndian(number: UInt) {
-        TODO("Not yet implemented")
+        val intValue = number.toInt()
+        val b1 = (intValue and 0xFF).toByte()
+        val b2 = (intValue shr 8 and 0xFF).toByte()
+        val b3 = (intValue shr 16 and 0xFF).toByte()
+        val b4 = (intValue shr 24 and 0xFF).toByte()
+        buffer.write(byteArrayOf(b4, b3, b2, b1))
     }
 
     override fun writeUint64(number: ULong) {
-        TODO("Not yet implemented")
+        val longValue = number.toLong()
+        return writeInt64(longValue);
     }
 
     override fun writeFloat32(number: Float) {
-        TODO("Not yet implemented")
+        val intBits = number.toBits()
+        for (i in 3 downTo 0) {
+            val byte = (intBits ushr (i * 8) and 0xFF).toByte()
+            buffer.write(byte.toInt())
+        }
     }
 
     override fun writeFloat64(number: Double) {
-        TODO("Not yet implemented")
+        val intBits = number.toBits()
+        for (i in 7 downTo 0) {
+            val byte = (intBits ushr (i * 8) and 0xFF).toByte()
+            buffer.write(byte.toInt())
+        }
     }
 
     override fun writeInt64(number: Long) {
-        TODO("Not yet implemented")
+        val longValue = number.toLong()
+        for (i in 7 downTo 0) {
+            val byte = (longValue ushr (i * 8) and 0xFF).toByte()
+            buffer.write(byte.toInt())
+        }
     }
 
     override fun writeVarUint8(number: UByte) {
-        TODO("Not yet implemented")
+        return writeVarUint64(number.toULong())
     }
 
     override fun writeVarUint16(number: UShort) {
-        TODO("Not yet implemented")
+        return writeVarUint64(number.toULong())
     }
 
     override fun writeVarUint32(number: UInt) {
-        TODO("Not yet implemented")
+        return writeVarUint64(number.toULong())
     }
 
     override fun writeVarUint64(number: ULong) {
-        TODO("Not yet implemented")
+        var n = number
+        do {
+            val bits = (n and 0x7Fu).toUByte()
+            n = n shr 7
+            val byte = if (n > 0uL) bits or 0x80u else bits
+            buffer.write(byte.toInt())
+        } while (n > 0uL)
     }
 
     override fun writeVarInt8(number: Byte) {
-        TODO("Not yet implemented")
+        return writeVarInt64(number.toLong())
     }
 
     override fun writeVarInt16(number: Short) {
-        TODO("Not yet implemented")
+        return writeVarInt64(number.toLong())
     }
 
     override fun writeVarInt32(number: Int) {
-        TODO("Not yet implemented")
+        return writeVarInt64(number.toLong())
     }
 
     override fun writeVarInt64(number: Long) {
-        TODO("Not yet implemented")
+        var n = number
+        val isNegative = n < 0
+        if (isNegative) {
+            n = -n
+        }
+
+        var firstByte = (0b0011_1111L and n).toUByte()
+        if (n > 0b0011_1111L) {
+            firstByte = firstByte or 0b1000_0000u
+        }
+        if (isNegative) {
+            firstByte = firstByte or 0b0100_0000u
+        }
+        writeUint8(firstByte)
+
+        n = n shr 6
+
+        while (n > 0) {
+            var b = 0u
+            if (n > 0b0111_1111L) {
+                b = b or 0b1000_0000u
+            }
+            b = b or (0b0111_1111L and n).toUInt()
+            writeUint8(b.toUByte())
+            n = n shr 7
+        }
     }
 
-    override fun writeVarUint8Array(number: UByteArray) {
-        TODO("Not yet implemented")
+    override fun writeUint8Array(array: UByteArray) {
+        buffer.write(array.toByteArray())
+    }
+
+    override fun writeVarUint8Array(array: UByteArray) {
+        writeVarUint32(array.size.toUInt())
+        writeUint8Array(array)
     }
 
     override fun writeVarString(str: String) {
-        TODO("Not yet implemented")
+        val utf8Bytes = str.toByteArray(StandardCharsets.UTF_8)
+        return writeVarUint8Array(utf8Bytes.toUByteArray())
     }
 
     override fun toBytes(): UByteArray {
         return buffer.toByteArray().toUByteArray()
     }
-
 }
 
--- lib0/src/test/kotlin/ByteArrayEncoderTest.kt
+++ lib0/src/test/kotlin/ByteArrayEncoderTest.kt
@@ -16,80 +16,204 @@ class ByteArrayEncoderTest {
 
     @Test
     fun writeUint8() {
-        assertEquals("01", encodeToHex<UByte>(1u) { e, num -> e.writeUint8(num) })
-        assertEquals("7b", encodeToHex<UByte>(123u) { e, num -> e.writeUint8(num) })
-        assertEquals("ff", encodeToHex<UByte>(255u) { e, num -> e.writeUint8(num) })
+        val write: (Encoder, UByte) -> Unit = { e, num -> e.writeUint8(num) }
+        assertEquals("01", encodeToHex(1u, write))
+        assertEquals("7b", encodeToHex(123u, write))
+        assertEquals("ff", encodeToHex(255u, write))
     }
 
     @Test
     fun writeUint16() {
+        val write: (Encoder, UShort) -> Unit = { e, num -> e.writeUint16(num) }
+        assertEquals("0000", encodeToHex(0u, write))
+        assertEquals("0100", encodeToHex(1u, write))
+        assertEquals("ff00", encodeToHex(255u, write))
+        assertEquals("ffff", encodeToHex(65535u, write))
     }
 
     @Test
     fun writeUint32() {
+        val write: (Encoder, UInt) -> Unit = { e, num -> e.writeUint32(num) }
+        assertEquals("00000000", encodeToHex(0u, write))
+        assertEquals("01000000", encodeToHex(1u, write))
+        assertEquals("ff000000", encodeToHex(255u, write))
+        assertEquals("ffff0000", encodeToHex(65535u, write))
+        assertEquals("ffffffff", encodeToHex(4294967295u, write))
     }
 
     @Test
     fun writeUint32BigEndian() {
+        val write: (Encoder, UInt) -> Unit = { e, num -> e.writeUint32BigEndian(num) }
+        assertEquals("00000000", encodeToHex(0u, write))
+        assertEquals("00000001", encodeToHex(1u, write))
+        assertEquals("000000ff", encodeToHex(255u, write))
+        assertEquals("0000ffff", encodeToHex(65535u, write))
+        assertEquals("ffffffff", encodeToHex(4294967295u, write))
     }
 
     @Test
     fun writeUint64() {
+        val write: (Encoder, ULong) -> Unit = { e, num -> e.writeUint64(num) }
+        assertEquals("0000000000000000", encodeToHex(0u, write))
+        assertEquals("0000000000000001", encodeToHex(1u, write))
+        assertEquals("00000000000000ff", encodeToHex(255u, write))
+        assertEquals("000000000000ffff", encodeToHex(65535u, write))
+        assertEquals("00000000ffffffff", encodeToHex(4294967295u, write))
+        assertEquals("ffffffffffffffff", encodeToHex(18446744073709551615u, write))
     }
 
     @Test
     fun writeFloat32() {
+        val write: (Encoder, Float) -> Unit = { e, num -> e.writeFloat32(num) }
+        assertEquals("00000000", encodeToHex(0f, write))
+        assertEquals("3f800000", encodeToHex(1f, write))
+        assertEquals("437f0000", encodeToHex(255f, write))
+        assertEquals("477fff00", encodeToHex(65535f, write))
+        assertEquals("4f800000", encodeToHex(4294967295f, write))
+        assertEquals("42f6e666", encodeToHex(123.45f, write))
+        assertEquals("496ffce2", encodeToHex(982990.1f, write))
     }
 
     @Test
     fun writeFloat64() {
+        val write: (Encoder, Double) -> Unit = { e, num -> e.writeFloat64(num) }
+        assertEquals("0000000000000000", encodeToHex(0.0, write))
+        assertEquals("3ff0000000000000", encodeToHex(1.0, write))
+        assertEquals("406fe00000000000", encodeToHex(255.0, write))
+        assertEquals("40efffe000000000", encodeToHex(65535.0, write))
+        assertEquals("41efffffffe00000", encodeToHex(4294967295.0, write))
+        assertEquals("43f0000000000000", encodeToHex(18446744073709552000.0, write))
+        assertEquals("405edccccccccccd", encodeToHex(123.45, write))
+        assertEquals("412dff9c33333333", encodeToHex(982990.1, write))
     }
 
     @Test
     fun writeInt64() {
+        val write: (Encoder, Long) -> Unit = { e, num -> e.writeInt64(num) }
+        assertEquals("0000000000000000", encodeToHex(0, write))
+        assertEquals("0000000000000001", encodeToHex(1, write))
+        assertEquals("ffffffffffffffff", encodeToHex(-1, write))
+        assertEquals("00000000000000ff", encodeToHex(255, write))
+        assertEquals("000000000000ffff", encodeToHex(65535, write))
+        assertEquals("ffffffffffff0001", encodeToHex(-65535, write))
+        assertEquals("8000000000000001", encodeToHex(-9223372036854775807, write))
+        assertEquals("7fffffffffffffff", encodeToHex(9223372036854775807, write))
     }
 
     @Test
     fun writeVarUint8() {
+        val write: (Encoder, UByte) -> Unit = { e, num -> e.writeVarUint8(num) }
+        assertEquals("00", encodeToHex(0u, write))
+        assertEquals("01", encodeToHex(1u, write))
+        assertEquals("ff01", encodeToHex(255u, write))
     }
 
     @Test
     fun writeVarUint16() {
+        val write: (Encoder, UShort) -> Unit = { e, num -> e.writeVarUint16(num) }
+        assertEquals("00", encodeToHex(0u, write))
+        assertEquals("01", encodeToHex(1u, write))
+        assertEquals("ff01", encodeToHex(255u, write))
+        assertEquals("ffff03", encodeToHex(65535u, write))
     }
 
     @Test
     fun writeVarUint32() {
+        val write: (Encoder, UInt) -> Unit = { e, num -> e.writeVarUint32(num) }
+        assertEquals("00", encodeToHex(0u, write))
+        assertEquals("01", encodeToHex(1u, write))
+        assertEquals("ff01", encodeToHex(255u, write))
+        assertEquals("ffff03", encodeToHex(65535u, write))
+        assertEquals("ffffffff0f", encodeToHex(4294967295u, write))
     }
 
     @Test
     fun writeVarUint64() {
+        val write: (Encoder, ULong) -> Unit = { e, num -> e.writeVarUint64(num) }
+        assertEquals("00", encodeToHex(0u, write))
+        assertEquals("01", encodeToHex(1u, write))
+        assertEquals("ff01", encodeToHex(255u, write))
+        assertEquals("ffff03", encodeToHex(65535u, write))
+        assertEquals("ffffffff0f", encodeToHex(4294967295u, write))
     }
 
     @Test
     fun writeVarInt8() {
+        val write: (Encoder, Byte) -> Unit = { e, num -> e.writeVarInt8(num) }
+        assertEquals("00", encodeToHex(0, write))
+        assertEquals("01", encodeToHex(1, write))
+        assertEquals("41", encodeToHex(-1, write))
     }
 
     @Test
     fun writeVarInt16() {
+        val write: (Encoder, Short) -> Unit = { e, num -> e.writeVarInt16(num) }
+        assertEquals("00", encodeToHex(0, write))
+        assertEquals("01", encodeToHex(1, write))
+        assertEquals("bf03", encodeToHex(255, write))
+        assertEquals("41", encodeToHex(-1, write))
+        assertEquals("ff03", encodeToHex(-255, write))
     }
 
     @Test
     fun writeVarInt32() {
+        val write: (Encoder, Int) -> Unit = { e, num -> e.writeVarInt32(num) }
+        assertEquals("00", encodeToHex(0, write))
+        assertEquals("01", encodeToHex(1, write))
+        assertEquals("bf03", encodeToHex(255, write))
+        assertEquals("bfff07", encodeToHex(65535, write))
+        assertEquals("41", encodeToHex(-1, write))
+        assertEquals("ff03", encodeToHex(-255, write))
+        assertEquals("ffff07", encodeToHex(-65535, write))
     }
 
     @Test
     fun writeVarInt64() {
+        val write: (Encoder, Long) -> Unit = { e, num -> e.writeVarInt64(num) }
+        assertEquals("00", encodeToHex(0, write))
+        assertEquals("01", encodeToHex(1, write))
+        assertEquals("bf03", encodeToHex(255, write))
+        assertEquals("bfff07", encodeToHex(65535, write))
+        assertEquals("bfffffff1f", encodeToHex(4294967295, write))
+        assertEquals("41", encodeToHex(-1, write))
+        assertEquals("ff03", encodeToHex(-255, write))
+        assertEquals("ffff07", encodeToHex(-65535, write))
+    }
+
+    @Test
+    fun writeUint8Array() {
+        val write: (Encoder, UByteArray) -> Unit = { e, str -> e.writeUint8Array(str) }
+        val data = ubyteArrayOf(0u, 1u, 128u, 254u)
+        assertEquals("000180fe", encodeToHex(data, write))
     }
 
     @Test
     fun writeVarUint8Array() {
+        val write: (Encoder, UByteArray) -> Unit = { e, str -> e.writeVarUint8Array(str) }
+        val data = ubyteArrayOf(0u, 1u, 128u, 254u)
+        assertEquals("04000180fe", encodeToHex(data, write))
     }
 
     @Test
     fun writeVarString() {
+        val write: (Encoder, String) -> Unit = { e, str -> e.writeVarString(str) }
+        assertEquals("0c48656c6c6f20576f726c6421", encodeToHex("Hello World!", write))
+        assertEquals("00", encodeToHex("", write))
+        assertEquals("12e4bda0e5a5bdefbc8ce4b896e7958cefbc81", encodeToHex("你好，世界！", write))
     }
 
     @Test
     fun toBytes() {
+        val encoder = ByteArrayEncoder(ByteArrayOutputStream())
+        encoder.writeUint8(12u)
+        encoder.writeVarString("12345，上山打老虎，one two three four five, climb a mountain and hit a tiger")
+        encoder.writeInt64(-9223372036854775807)
+
+        val bytes = encoder.toBytes()
+        assertEquals(
+            "0c533132333435efbc8ce4b88ae5b1b1e68993e88081e8998eefbc8c6f6e652074776f20746872656520666f757220666976652c20636c696d622061206d6f756e7461696e20616e642068697420612074696765728000000000000001",
+            bytes.toHexString()
+        )
+
     }
 }
